c2188cbf65835bc5b43b6409a7983fe09ff48abb
DANNYOBANDO1 <danny-111999@hotmail.com>
2024-11-25 18:11:54 -0500

    se agrega el proyecto

---
+++ juego de la serpiente.py
@@ -0,0 +1,13 @@
+"""# Pantalla del videojuego
+screen = screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("negro")
+screen.title("el juego de la serpiente")
+screen.tracer(0) """
+
+# Pantalla
+screen = Screen()
+screen.setup(width=1920, height=1080)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
